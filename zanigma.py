62b51727cd9b87c0c8d7da45c78f4b5525ad574e
utepshaadm <utep.shaadm@gmail.com>
2026-08-01 18:36:16 -0700

    Create zanigma.py

---
+++ zanigma.py
@@ -0,0 +1,49 @@
+''' Zanigma Card Cipher'''
+''' by KryptoMagick (Karl Zander) '''
+from random import shuffle
+
+class ZANIGMA:
+    def __init__(self):
+        self.red = list(range(26))
+        self.black = list(range(26))
+        
+    def gen_rand_decks(self):
+    	shuffle(self.red)
+    	shuffle(self.black)
+    	
+    def step(self):
+         self.red.append(self.red.pop(0))
+         self.black.append(self.black.pop(0))
+         for x in range(self.red[0]):
+              self.black.append(self.black.pop(0))
+
+         for x in range(self.black[0]):
+              self.red.append(self.red.pop(0))
+    		
+    def encrypt_letter(self, letter):
+        self.step()
+        num = ord(letter) - 65
+        num = self.red[num]
+        num = self.black[num]
+        return chr(num + 65)
+        
+    def decrypt_letter(self, letter):
+        self.step()
+        num = ord(letter) - 65
+        num = self.black.index(num)
+        num = self.red.index(num)
+        return chr(num + 65)
+
+    def encrypt(self, letters):
+        ctxt = []
+        for x in range(len(letters)):
+            letter = self.encrypt_letter(letters[x])
+            ctxt.append(letter)
+        return "".join(ctxt)
+
+    def decrypt(self, letters):
+        ptxt = []
+        for x in range(len(letters)):
+            letter = self.decrypt_letter(letters[x])
+            ptxt.append(letter)
+        return "".join(ptxt)
